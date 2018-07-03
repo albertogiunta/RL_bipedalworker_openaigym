cd63f0c9b977f2fed0484b9eda9e14a8e3e676a7
albertogiunta <alb.giunta@gmail.com>
2018-07-03 11:10:14 +0200

    added model

--- bipedal_agent/agent.py
+++ bipedal_agent/agent.py
@@ -1,13 +1,29 @@
+from keras import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import numpy as np
+import gym.spaces
 import gym
-from keras.models import Sequential
 
 class BipedalAgent(object):
 
-    def __init__(self, action_space):
+    def __init__(self, action_space, state):
         self.action_space = action_space
+        self.state_size = len(state)
+        self.action_size = len(action_space)
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
 
     def act(self, observation, reward, done):
-        print "TODO"
+        return self.action_space.sample()
 
 
 if __name__ == '__main__':
@@ -15,7 +31,10 @@ if __name__ == '__main__':
     env.seed(0)
     env.render()
 
-    agent = BipedalAgent(env.action_space)
+    print env.action_space
+    state = np.reshape(env.reset(), [1, 24])
+    agent = BipedalAgent(env.action_space, state)
+
 
     episode_count = 100
     reward = 0
@@ -26,6 +45,7 @@ if __name__ == '__main__':
         while True:
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
+            env.render()
             if done:
                 break
 
