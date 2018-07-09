d4a2e5cf0b01e936d3094449da3ef2bda95f21a5
albertogiunta <alb.giunta@gmail.com>
2018-07-09 12:14:13 +0200

    indentato quello che la lisa non indenta

--- bipedal_agent/agent.py
+++ bipedal_agent/agent.py
@@ -10,6 +10,7 @@ from collections import deque
 
 EPISODES = 1000
 
+
 class BipedalAgent(object):
 
     def __init__(self, action_space):
@@ -18,7 +19,7 @@ class BipedalAgent(object):
 
         self.step_size = 0.5
         self.joint_number = 4
-        self.action_size = 2/self.step_size * self.joint_number
+        self.action_size = 2 / self.step_size * self.joint_number
 
         self.memory = deque(maxlen=2000)
 
@@ -39,36 +40,36 @@ class BipedalAgent(object):
         return model
 
     def take_four_maxes(self, array):
-        #Matrice 4*3, dove ogni riga contiene [ reward, azione ottima, indice dell'azione ] per ogni giunto
+        # Matrice 4*3, dove ogni riga contiene [ reward, azione ottima, indice dell'azione ] per ogni giunto
         maxes = np.zeros(4, 3)
         half = len(array) / 2
         for joint_index, a in np.reshape(array, (4, 20)):
 
-            #Indice sull'array di 20 dell'elemento con reward massima
+            # Indice sull'array di 20 dell'elemento con reward massima
             max_index = np.argmax(a)
 
-            #Salvo la reward massima trovata per il giunto
+            # Salvo la reward massima trovata per il giunto
             maxes[joint_index][0] = a[max_index]
 
-            #Normalizza l'indice considerando solo la meta' positiva dell'intervallo (0-1) per poter ragionare
-            #una sola volta a prescindere dal segno
+            # Normalizza l'indice considerando solo la meta' positiva dell'intervallo (0-1) per poter ragionare
+            # una sola volta a prescindere dal segno
             if max_index < half:
-                #Es: l'indice di partenza 1 diventa |1-2|-1 = 0
+                # Es: l'indice di partenza 1 diventa |1-2|-1 = 0
                 partial_index = np.abs(max_index - half) - 1
             else:
-                #Nel caso positivo non e' necessario sottrarre l'1  Es: |2-2| = 0
+                # Nel caso positivo non e' necessario sottrarre l'1  Es: |2-2| = 0
                 partial_index = max_index - half
 
-            #Calcolo l'azione media del bucket ottenuto come (lower_bound + upper_bound)/2
+            # Calcolo l'azione media del bucket ottenuto come (lower_bound + upper_bound)/2
             middle_bucket = ((partial_index * self.step_size) + (partial_index * self.step_size + self.step_size)) / 2
 
-            #Se l'indice era dell'intervallo negativo (-1, 0) allora l'azione sara' negativa
+            # Se l'indice era dell'intervallo negativo (-1, 0) allora l'azione sara' negativa
             if max_index < half:
                 middle_bucket = -middle_bucket
 
-            #Salvo l'azione media come azione ottimale
+            # Salvo l'azione media come azione ottimale
             maxes[joint_index][1] = middle_bucket
-            #Salvo l'indice globale (su 80) dell'azione ottimale
+            # Salvo l'indice globale (su 80) dell'azione ottimale
             maxes[joint_index][2] = max_index + (joint_index * max_index)
 
         print(maxes)
@@ -81,7 +82,7 @@ class BipedalAgent(object):
 
         print "NOT random"
         act_values = self.model.predict(state)
-        #Restituiamo solo l'array che definisce l'azione ottima globale
+        # Restituiamo solo l'array che definisce l'azione ottima globale
         return np.transpose(self.take_four_maxes(act_values))[1]
 
     def remember(self, state, action, reward, next_state, done):
@@ -90,22 +91,22 @@ class BipedalAgent(object):
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            #Inizializza q_reward come un array di 4 elementi uguali
+            # Inizializza q_reward come un array di 4 elementi uguali
             q_reward = [reward] * 4
             if not done:
-                #Prendiamo le reward dell'azione ottima allo stato successivo
+                # Prendiamo le reward dell'azione ottima allo stato successivo
                 maxes = self.take_four_maxes(self.model.predict(next_state)[0])
-                #Aggiorniamo la nostra q_reward con la Q function
+                # Aggiorniamo la nostra q_reward con la Q function
                 q_reward = (reward + self.gamma * (maxes[0]))
 
-            #Prendo l'output che la rete mi da attualmente
+            # Prendo l'output che la rete mi da attualmente
             nn_output = self.model.predict(state)
 
-            #Modifico l'output ottenuto con la q_reward calcolata
+            # Modifico l'output ottenuto con la q_reward calcolata
             for index, max in maxes:
                 nn_output[0][max[2]] = q_reward[index]
 
-            #Addestro la rete con l'output ricalcolato
+            # Addestro la rete con l'output ricalcolato
             self.model.fit(state, nn_output, epochs=1, verbose=0)
 
         if self.epsilon > self.epsilon_min:
